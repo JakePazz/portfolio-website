db12ddde0628993abcfd129af1de56ab132c75e1
Jake <jake.pazzard@gmail.com>
2025-01-18 01:27:33 +0000

    Moved comp. to /lib and added hover styles to Button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { children } = $props();
+
+</script>
+
+<button
+class="
+px-4 py-1 bg-primary/5 text-textDim rounded-lg inline-flex gap-2 items-center justify-center
+hover:text-text hover:bg-primary/20 hover:scale-105
+transition ease-in-out duration-300
+"
+>
+{@render children()}
+</button>
