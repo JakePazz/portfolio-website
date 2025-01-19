ffec698f9d06a65cd4519537892edc87895d7d6a
Jake <jake.pazzard@gmail.com>
2025-01-19 23:10:12 +0000

    Add onclick event to Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  let { children } = $props();
+  let { children, onclick } = $props();
 
 </script>
 
 <button
+onclick={onclick}
 class="
 px-4 py-1 bg-primary/5 text-textDim rounded-lg inline-flex gap-2 items-center justify-center
 hover:text-text hover:bg-primary/20 hover:scale-105
