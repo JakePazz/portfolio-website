0afd064ec308b548f52380046364e4d0da841002
Jake <jake.pazzard@gmail.com>
2025-01-25 01:13:46 +0000

    Skill component cleanup

--- src/lib/components/Skill.svelte
+++ src/lib/components/Skill.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import type { Tskill } from "$lib/types/skill";
-	import { fade } from "svelte/transition";
 
   const { skill }: {skill: Tskill} = $props()
 
   let hovering = $state(false)
-
 </script>
 
 <div class="relative inline-block">
@@ -17,9 +15,11 @@
   onmouseleave={() => {
     hovering = false
   }}
+
   onclick={() => {
     window.open(skill.link, "_blank")
   }}
+  
   aria-label={skill.name}
   class="
   bg-secondary/15 rounded-lg px-2 py-2 inline-flex items-center justify-center gap-3
@@ -34,8 +34,4 @@
     {/if}
     
   </button>
-
-  <!-- {#if hovering}
-    <div class="absolute top-full left-0 bg-secondary/15 rounded-lg p-2 mt-2" transition:fade >{skill.name}</div>  
-  {/if} -->
 </div>
