a382c18f843f85b5cb1d6319bc7b18c7809e97a1
Jake <jake.pazzard@gmail.com>
2025-01-25 01:13:26 +0000

    Updated base layout - minor changes
    Changed copyright footer text content
    Put in missing onclick properties to buttons to work inline with previous addition to the Button component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,15 @@
 
 <main class="w-full">
 	<header class="h-[4rem] box-border w-full px-6 pt-6 flex items-center justify-end gap-3">
-    <Button><DocumentAttachmentIcon size={34} color="rgba(var(--accent))"/> <p class="text-xl">CV</p> </Button>
-    <Button><Sun02Icon size={34} color="rgba(var(--accent))"  /></Button>
+    <Button onclick={() => {}} ><DocumentAttachmentIcon size={34} color="rgba(var(--accent))"/> <p class="text-xl">CV</p> </Button>
+    <Button onclick={() => {}}><Sun02Icon size={34} color="rgba(var(--accent))"  /></Button>
   </header>
+
 	<div class="px-16">
 		{@render children()}
 	</div>
+
 	<footer class="py-6 flex items-center justify-center">
-		<p class="text-textDim">&copy; <a class="hover:underline" target="_blank" href="https://github.com/JakePazz">JakePazz</a> 2025</p>
+		<p class="text-textDim">&copy; 2025 <a class="hover:underline" target="_blank" href="https://github.com/JakePazz">Jake Pazzard</a></p>
 	</footer>
 </main>
