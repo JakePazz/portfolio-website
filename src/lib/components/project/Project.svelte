b8ef616ace2beae4f2f6efdb1931454d356f015e
Jake <jake.pazzard@gmail.com>
2025-01-27 00:02:25 +0000

    Project component additions
    Moved blur to change with transition using tw classes
    Added overlayed buttons to control future image carousel
    Added skills section

--- src/lib/components/project/Project.svelte
+++ src/lib/components/project/Project.svelte
@@ -3,21 +3,47 @@
 	import { CldImage } from "svelte-cloudinary";
 	import Container from "../Container.svelte";
 	import { formatDate } from "$lib/functions/formatDate";
+	import Button from "../Button.svelte";
+	import { ArrowLeft01Icon, ArrowRight01Icon } from "hugeicons-svelte";
+	import { fly } from "svelte/transition";
+
+  // Data
+  import { knownSkills, learningSkills } from "$lib/data/skills"
+	import Skill from "../Skill.svelte";
+
+  const skills = knownSkills.concat(learningSkills)
+
 
   const { project }: { project: Tproject } = $props()
+
+  let hoveringImage: boolean = $state(false)
 
 </script>
 
 <Container twStyles="flex gap-4">
-  <div class="flex-[3]">  
+  <div
+  role="group"
+  class="relative flex-[3] max-w-fit"
+  onmouseenter={() => {hoveringImage = true}}
+  onmouseleave={() => {hoveringImage = false}}
+  >
+    {#if hoveringImage}
+      <span transition:fly={{duration: 200, x: -20}} class="absolute top-1/2 left-4 z-20">
+        <Button onclick><ArrowLeft01Icon color="rgba(var(--accent))" size={30}/></Button>
+      </span>
+    {/if}
     <CldImage
       src={`projects/${project.imagesFolder}/0`}
       width={1600}
       height={900}
       crop="fill_pad"
-      blur="600"
-      class="rounded-2xl"
+      class="rounded-3xl transition-all z-10 {hoveringImage ? "" : "blur-sm"}"
     />
+    {#if hoveringImage}
+      <span transition:fly={{duration: 200, x: 20}} class="absolute top-1/2 right-4 z-20">
+        <Button onclick><ArrowRight01Icon color="rgba(var(--accent))" size={30}/></Button>
+      </span>
+    {/if}
   </div>
 
   <div class="flex-1">
@@ -41,12 +67,19 @@
       {/if}
     </div>
 
-    <h4 class="text-3xl text-medium mb-4">{project.title}</h4>
+    <h4 class="text-3xl mb-4">{project.title}</h4>
 
     <p class="text-textDim ">{project.description}</p>
 
     {#if project.skills}
-      
+      <h5 class="text-2xl mt-6 font-medium">Skills</h5>
+      <div class="flex flex-wrap gap-2">
+        {#each project.skills as projectSkill}
+          {#if skills.find((skill) => skill.id === projectSkill)}
+            <Skill skill={skills.find((skill) => skill.id === projectSkill)!!} />
+          {/if}
+        {/each}
+      </div>
     {/if}
   </div>
 </Container>
