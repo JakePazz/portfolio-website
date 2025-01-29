c9f62d98f72a386c3c6332d9d5e5cf7cae9ae574
Jake <jake.pazzard@gmail.com>
2025-01-29 23:31:21 +0000

    CV button functionality
    CV url currently stored within personalInformation file
    Also updated Cloudinary image path to include parent /portfolio folder now added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,20 @@
 	import { DocumentAttachmentIcon, Sun02Icon } from "hugeicons-svelte";
 	import Button from "$lib/components/Button.svelte"
 
+	import { personalInformation } from '$lib/data/personalInformation';
+
 	let { children } = $props();
+
+	function openCV() {
+		window.open(personalInformation.cv, "_blank")
+	}
+
 </script>
 
 <main class="w-full">
 	<header class="h-[4rem] box-border w-full px-6 pt-6 flex items-center justify-end gap-3">
-    <Button onclick={() => {}} ><DocumentAttachmentIcon size={34} color="rgba(var(--accent))"/> <p class="text-xl">CV</p> </Button>
-    <Button onclick={() => {}}><Sun02Icon size={34} color="rgba(var(--accent))"  /></Button>
+    <Button onclick={openCV} ><DocumentAttachmentIcon size={34} color="rgba(var(--accent))"/> <p class="text-xl">CV</p> </Button>
+    <Button onclick={() => {}}><Sun02Icon size={34} color="rgba(var(--accent))"/></Button>
   </header>
 
 	<div class="px-16">
