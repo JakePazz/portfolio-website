a2beb6d0f6305f23df137a2d74a37ff13202f428
Jake <jake.pazzard@gmail.com>
2025-02-01 00:28:21 +0000

    Added twStyles pass through to Button comp

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-  let { children, onclick } = $props();
+  let { children, onclick, twStyles = "" } = $props();
 
 </script>
 
 <button
 onclick={onclick}
-class="
+class={`
 px-4 py-1 bg-primary/5 text-textDim rounded-lg inline-flex gap-2 items-center justify-center
 hover:text-text hover:bg-primary/20 hover:scale-105
 transition ease-in-out duration-300
-"
+${twStyles}
+`}
 >
 {@render children()}
 </button>
