4cae6cfdc43e06dcab56268cf4e77be3f7178cad
Jake <jake.pazzard@gmail.com>
2025-02-02 02:25:49 +0000

    Deleted data files

--- src/lib/data/personalInformation.ts
+++
@@ -1,7 +0,0 @@
-export const personalInformation = {
-  name: "Jake Pazzard",
-  email: "jake.pazzard@gmail.com",
-  github: "https://github.com/JakePazz",
-  linkedin: "https://www.linkedin.com/in/jake-pazzard/",
-  cv: "https://res.cloudinary.com/dvscysrba/image/upload/v1738192865/cv.pdf"
-}
