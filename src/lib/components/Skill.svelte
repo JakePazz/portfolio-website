0ec7cedd7cc6f248d3727b3e6d09d604cad0a24c
Jake <jake.pazzard@gmail.com>
2025-02-03 23:38:00 +0000

    Update Skill component
    Removed name display on hover

--- src/lib/components/Skill.svelte
+++ src/lib/components/Skill.svelte
@@ -2,36 +2,19 @@
 	import type { Tskill } from "$lib/types/skill";
 
   const { skill }: {skill: Tskill} = $props()
-
-  let hovering = $state(false)
 </script>
 
 <div class="relative inline-block">
   <button
-  onmouseenter={() => {
-    hovering = true
-  }}
-
-  onmouseleave={() => {
-    hovering = false
-  }}
-
   onclick={() => {
     window.open(skill.link, "_blank")
   }}
-  
   aria-label={skill.name}
   class="
   bg-secondary/15 rounded-lg px-2 py-2 inline-flex items-center justify-center gap-3
   transition-all
   hover:scale-105 hover:cursor-pointer hover:bg-secondary/25
   ">
-    {#if !hovering}
-      <i class="text-5xl {skill.deviconName}"></i>
-    {:else}
-      <i class="text-5xl {skill.deviconName}" ></i>  
-      <p>{skill.name}</p>
-    {/if}
-    
+    <i class="text-5xl {skill.deviconName}"></i>
   </button>
 </div>
